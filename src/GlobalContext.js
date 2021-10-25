646989ed9859d8fae3700fd8a6e8f890c5d7a8bc
Akshat Gupta <akshatvg23@gmail.com>
2021-10-26 01:37:17 +0530

    token support added

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -1,45 +1,39 @@
-import { useState, useRef, createContext, useContext } from 'react'
+import {useState, useRef, createContext, useContext} from 'react'
 
 const UserContext = createContext()
 const StartContext = createContext()
 const ClientContext = createContext()
 
-
 export const useUsers = () => {
     return useContext(UserContext)
 }
-
 
 export const useStart = () => {
     return useContext(StartContext)
 }
 
-
 export const useClient = () => {
     return useContext(ClientContext)
 }
 
-
-export const GlobalProvider = ({ children }) => {
+export const GlobalProvider = ({children}) => {
 
     const [users, setUsers] = useState([])
     const [start, setStart] = useState(false)
-    const rtc = useRef({
-        // For the local client.
+    const rtc = useRef({ // For the local client.
         client: null,
         // For the local audio and video tracks.
         localAudioTrack: null,
-        localVideoTrack: null,
+        localVideoTrack: null
     });
 
-
-    return (
-        <ClientContext.Provider value = {rtc}>
-            <UserContext.Provider value={[users, setUsers]}>
-                <StartContext.Provider value={[start, setStart]}>
-                    {children}
-                </StartContext.Provider>
-            </UserContext.Provider>
-        </ClientContext.Provider>
-    )
+    return (<ClientContext.Provider value={rtc}>
+        <UserContext.Provider value={
+            [users, setUsers]
+        }>
+            <StartContext.Provider value={
+                [start, setStart]
+            }> {children} </StartContext.Provider>
+        </UserContext.Provider>
+    </ClientContext.Provider>)
 }
